44424e7b9ee11e7b46d4fb5494039cd34ef5526d
smsarov <smsarov@gmail.com>
2024-05-01 15:23:28 +0300

    please work on vercel...

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 
-const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL);
-// const socket = io('http://localhost:8000');
+// const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL);
+const socket = io('https://smsarov.ru/');
 
 export {socket}
